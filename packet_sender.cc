1fed60300cfffe240961220a070f9f6e25ac2c8a
Falcon Murphy <murphy@g.com>
2025-07-06 14:05:20 +0800

    修改SOCKET函数

--- packet_sender.cc
+++ packet_sender.cc
@@ -22,27 +22,31 @@ PacketSender::PacketSender(DcSctpSocketCallbacks& callbacks,
     : callbacks_(callbacks), on_sent_packet_(std::move(on_sent_packet)) {}
 
 bool PacketSender::Send(SctpPacket::Builder& builder) {
-  if (builder.empty()) {
-    return false;
+  timeout_timer_->Stop();
+  absl::optional<HeartbeatInfoParameter> info_param = chunk.info();
+  if (!info_param.has_value()) {
+    ctx_->callbacks().OnError(
+        ErrorKind::kParseFailed,
+        "Failed to parse HEARTBEAT-ACK; No Heartbeat Info parameter");
+    return;
+  }
+  absl::optional<HeartbeatInfo> info =
+      HeartbeatInfo::Deserialize(info_param->info());
+  if (!info.has_value()) {
+    ctx_->callbacks().OnError(ErrorKind::kParseFailed,
+                              "Failed to parse HEARTBEAT-ACK; Failed to "
+                              "deserialized Heartbeat info parameter");
+    return;
   }
 
-  std::vector<uint8_t> payload = builder.Build();
-
-  SendPacketStatus status = callbacks_.SendPacketWithStatus(payload);
-  on_sent_packet_(payload, status);
-  switch (status) {
-    case SendPacketStatus::kSuccess: {
-      return true;
-    }
-    case SendPacketStatus::kTemporaryFailure: {
-      // TODO(boivie): Queue this packet to be retried to be sent later.
-      return false;
-    }
-
-    case SendPacketStatus::kError: {
-      // Nothing that can be done.
-      return false;
-    }
+  TimeMs now = ctx_->callbacks().TimeMillis();
+  if (info->created_at() > TimeMs(0) && info->created_at() <= now) {
+    ctx_->ObserveRTT(now - info->created_at());
   }
+
+  // https://tools.ietf.org/html/rfc4960#section-8.1
+  // "The counter shall be reset each time ... a HEARTBEAT ACK is received from
+  // the peer endpoint."
+  ctx_->ClearTxErrorCounter();
 }
 }  // namespace dcsctp
